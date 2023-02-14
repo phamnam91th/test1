e74219777283a331507b3b32de5e8243498f66bf
phamnam91th <83792539+phamnam91th@users.noreply.github.com>
2023-02-14 07:43:31 +0000

    done

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./style.css">
+    <title>Home</title>
+</head>
+<body>
+    <div class="container">
+        <header>
+            <div class="logo">
+                <img src="https://www.freepnglogos.com/uploads/apple-logo-png/apple-logo-png-dallas-shootings-don-add-are-speech-zones-used-4.png" alt="">
+            </div>
+            <nav>
+                <ul>
+                    <li>HOME</li>
+                    <li>ABOUT</li>
+                    <li>PRODUCT</li>
+                    <li>NEW</li>
+                    <li>CONTACT</li>
+                </ul>
+            </nav>
+        </header>
+        <section>
+            <div class="banner">
+                <img src="https://anhdepfree.com/wp-content/uploads/2022/11/anh-nen-gai-xinh-2k-dep-cho-may-tinh_70885363113.jpg" alt="">
+            </div>
+        </section>
+
+    </div>
+</body>
+</html>
